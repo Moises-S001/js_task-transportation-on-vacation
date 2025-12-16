7bb8138d08c2ff54c3412d51e771e64a40d1d832
moises <carlosmoises.s.s02@gmail.com>
2025-12-16 20:09:46 -0300

    task_transportation-on-vacation solution 01

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,31 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DESCONT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DESCONT = 20;
+
+  let totalPreci = null;
+
+  if (days >= LONG_TERM) {
+    totalPreci = days * PRICE_DAY;
+
+    return totalPreci - LONG_TERM_DESCONT;
+  } else if (days >= SHORT_TERM) {
+    totalPreci = days * PRICE_DAY;
+
+    return totalPreci - SHORT_TERM_DESCONT;
+  } else if (days < 3) {
+    totalPreci = days * PRICE_DAY;
+
+    return totalPreci;
+  }
+
+  return totalPreci;
 }
 
 module.exports = calculateRentalCost;
